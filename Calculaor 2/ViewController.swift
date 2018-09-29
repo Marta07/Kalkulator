b6cdd55d1b95dc60447b2895127d20b202ddae3a
Marta Stolevska <stolevska.marta@gmail.com>
2018-09-29 15:14:49 +0200

    Initial commit

---
+++ Calculaor 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculaor 2
+//
+//  Created by CodeWell on 9/26/18.
+//  Copyright © 2018 CodeWell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
